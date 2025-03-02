2b8c360381a5b9043b4940c03af08864593cd01a
Om Mishra <ommishra.work@gmail.com>
2025-03-02 18:04:44 +0530

    backend: Configured core urls
    Signed-off-by: Om Mishra <om.mishra@bacancy.com>

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from core.viewsets import DocumentViewSet
+
+router = DefaultRouter()
+router.register(r'documents', DocumentViewSet, basename='documents')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', include('core.urls')),
+    path('api/v1/', include(router.urls)),
 ]
